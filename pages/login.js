d3af89b5672c1ec885cbcee2ca631fe7516e32d1
Jai2899 <gjayasriram@gmail.com>
2021-10-20 22:25:26 -0400

    Initial FrontEnd Code

---
+++ pages/login.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+const Login = () => {
+  return (
+    <>
+      <Head>
+        <title>Title Genius | Login</title>
+        <meta name="keywords" content="title-genius" />
+      </Head>
+      <div className="dummy">
+        <h1>DUMMY PAGE</h1>
+        <h1>This is a Login Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default Login;
