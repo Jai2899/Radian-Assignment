bf758c43b3380c0039a8da17769f3b7b0fa87426
unknown <you@example.com>
2021-11-02 21:29:20 +0530

    Temp

--- comps/NavbarRight.js
+++ comps/NavbarRight.js
@@ -1,18 +1,19 @@
 import Link from "next/link";
+
 const NavbarRight = () => {
-    return ( 
-        <div className="nav-ele-right">
-          <Link href="/search">
-            <a>Search</a>
-          </Link>
-          <Link href="/login">
-            <a>Login</a>
-          </Link>
-          <Link href="/getaquote/">
-            <a>Get a Quote</a>
-          </Link>
-        </div>
-     );
+  return (
+    <div className="nav-ele-right">
+      <Link href="/search">
+        <a>Search</a>
+      </Link>
+      <Link href="/login">
+        <a>Login</a>
+      </Link>
+      <Link href="/getaquote/">
+        <a>Get a Quote</a>
+      </Link>
+    </div>
+  );
 }
- 
+
 export default NavbarRight;
--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,44 @@
 import Head from "next/head";
+import { useState, useRef } from 'react';
+
 const Login = () => {
-  return (
-    <>
-      <Head>
-        <title>Title Genius | Login</title>
-        <meta name="keywords" content="title-genius" />
-      </Head>
-      <div className="dummy">
-        <h1>DUMMY PAGE</h1>
-        <h1>This is a Login Page</h1>
-      </div>
-    </>
-  );
+    const [isLogin, setIsLogin] = useState(true);
+
+    const switchAuthModeHandler = () => {
+        setIsLogin((prevState) => !prevState);
+    };
+
+    return (
+        <>
+            <Head>
+                <title>Title Genius | Login</title>
+                <meta name="keywords" content="title-genius" />
+            </Head>
+            <div className="dummy">
+                <form>
+                    <div>
+                        <label>{isLogin ? 'Login' : 'Create Account'}</label>
+                        <br />
+                        <label htmlFor='email'>Your Email</label>
+                        <input type='email' id='email' required />
+                    </div>
+                    <div>
+                        <label htmlFor='password'>Your Password</label>
+                        <input type='password' id='password' required />
+                    </div>
+                    <div>
+                        <button>{isLogin ? 'Login' : 'Create Account'}</button>
+                        <button
+                            type='button'
+                            onClick={switchAuthModeHandler}
+                        >
+                            {isLogin ? 'Create new account' : 'Login with existing account'}
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
 };
 
 export default Login;
